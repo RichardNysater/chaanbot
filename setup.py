44a6b727b542bd2fe886d08ddd8c0d396bab4d51
RichardNysater <richard@nysater.org>
2021-11-23 09:13:50 +0100

    Fix replay message bug and update dependencies

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
